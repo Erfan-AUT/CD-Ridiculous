82dbaa186285afc6e01c1b8badbfb89f4e823bd4
shayandesh <shafaghi1378@gmail.com>
2020-10-31 17:31:25 +0330

    Regex for matching keywords and ID.

--- compiler/lex.py
+++ compiler/lex.py
@@ -52,3 +52,11 @@ tokens = tuple(reserved.values()) + (
     'COMMA',
     'ERROR!'
 )
+
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value,'ID')    # Check for reserved words
+    return t
+
+
