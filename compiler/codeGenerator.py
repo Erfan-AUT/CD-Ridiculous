66618d6de581e80a4e06746e947ee0f738beb08c
Erfan Abedi <erfanabedi.78@gmail.com>
2021-01-17 17:53:35 +0330

    remove prints.

--- compiler/codeGenerator.py
+++ compiler/codeGenerator.py
@@ -7,7 +7,7 @@ class CodeGenerator:
     def arithmetic_code(p, temp):
         p[0] = NonTerminal()
         p[0].in_place = temp
-        p[0].code = p[0].place + " = "
+        p[0].code = p[0].in_place + " = "
         p[0].code += p[1].replacement() + " " + p[2] + " " + p[3].replacement()
         print(p[0].code)
 
--- compiler/parser.py
+++ compiler/parser.py
@@ -21,34 +21,29 @@ def new_temp():
     return "T" + str(tempCount)
 
 
-# def str_to_class(class_name):
-    # return eval(class_name)
-
-
 ############# Parser Methods ################
 
 
 def p_program(p):
     "program : declist MAIN LRB RRB block"
-    print("p_program")
+    pass
 
 
 def p_declist(p):
     """declist : declist dec
     | eps"""  # eps is equal to epsilon
-    print("p_declist")
+    pass
 
 
 def p_dec(p):
     """dec : vardec
     | funcdec"""
-    print("p_dec")
+    pass
 
 
 def p_vardec(p):
     "vardec : idlist COLON type SEMICOLON"
-    p[0] = NonTerminal()
-    p[0].explicit_type = p[3].explicit_type
+    p[0] = p[3]
     p[0].code = p[0].explicit_type + " " + p[1].code + p[4]
     print(p[0].code)
 
@@ -56,7 +51,7 @@ def p_vardec(p):
 def p_funcdec(p):
     """funcdec : FUNCTION ID LRB paramdecs RRB COLON type block
     | FUNCTION ID LRB paramdecs RRB block"""
-    print("p_funcdec")
+    pass
 
 
 def p_type(p):
@@ -68,62 +63,59 @@ def p_type(p):
 
 
 def p_iddec(p):
-    """iddec : lvalue ASSIGN exp
-    """
+    """iddec : lvalue ASSIGN exp"""
     p[0] = NonTerminal()
     p[0].code = p[1].code + p[2] + p[3].code
 
-def p_single_iddec(p):
-    "iddec : lvalue" 
-    p[0] = NonTerminal()
-    p[0].code = p[1].code
+
+def p_iddec_single(p):
+    "iddec : lvalue"
+    p[0] = p[1]
 
 
 def p_idlist(p):
-    """idlist : idlist COMMA iddec
-    """
-    print("p_idlist")
+    """idlist : idlist COMMA iddec"""
     p[0] = NonTerminal()
     p[0].code = p[1].code + p[2] + p[3].code
 
-def p_single_idlist(p):
+
+def p_idlist_single(p):
     "idlist : iddec"
-    p[0] = NonTerminal()
-    p[0].code = p[1].code
+    p[0] = p[1]
 
 
 def p_paramdecs(p):
     """paramdecs : paramdecslist
     | eps"""
-    print("p_paramdecs")
+    pass
 
 
 def p_paramdecslist(p):
     """paramdecslist : paramdec
     | paramdecslist COMMA paramdec
     """
-    print("p_paramdecslist")
+    pass
 
 
 def p_paramdec(p):
     """paramdec : ID LSB RSB COLON type"""
-    print("p_paramdec")
+    pass
 
 
-def p_single_paramdec(p):
+def p_paramdec_single(p):
     "paramdec : ID COLON type"
     pass
 
 
 def p_block(p):
     "block : LCB stmtlist RCB"
-    print("p_block")
+    pass
 
 
 def p_stmtlist(p):
     """stmtlist : stmtlist stmt
     | eps"""
-    print("p_stmtlist")
+    pass
 
 
 def p_lvalue(p):
@@ -132,27 +124,29 @@ def p_lvalue(p):
     p[0] = NonTerminal()
     p[0].code = p[1] + p[2] + p[3].code + p[4]
 
-def p_single_lvalue(p):
+
+def p_lvalue_single(p):
     "lvalue : ID"
     p[0] = NonTerminal()
     p[0].code = p[1]
 
+
 def p_case(p):
     "case : WHERE const COLON stmtlist"
-    print("p_case")
+    pass
 
 
 def p_cases(p):
     """cases : cases case
     | eps
     """
-    print("p_cases")
+    pass
 
 
 def p_stmt(p):
     """stmt : ostmt
     | cstmt"""
-    print("p_stmt")
+    pass
 
 
 def p_ostmt(p):
@@ -177,7 +171,7 @@ def p_cstmt(p):
 def p_elseiflist(p):
     """elseiflist : elseiflist ELSEIF LRB exp RRB cstmt
     | eps"""
-    print("p_elseiflist")
+    pass
 
 
 def p_simple(p):
@@ -185,9 +179,15 @@ def p_simple(p):
     | exp SEMICOLON
     | block
     | vardec
-    | ON LRB exp RRB LCB cases RCB SEMICOLON
-    | PRINT LRB ID RRB SEMICOLON"""
-    print("p_stmt")
+    | ON LRB exp RRB LCB cases RCB SEMICOLON"""
+    pass
+
+
+def p_print(p):
+    "simple : PRINT LRB ID RRB SEMICOLON"
+    p[0] = NonTerminal()
+    p[0].code = """printf("%d", {});""".format(p[3])
+    pass
 
 
 def p_relop(p):
@@ -197,35 +197,50 @@ def p_relop(p):
     | LE
     | EQ
     | NE"""
-    print("p_relop")
+    pass
 
 
 def p_exp(p):
-    """exp : exp relop exp %prec LT
-    | lvalue %prec OR
-    | SUB exp
-    | NOT exp"""
-    print("p_exp")
+    """exp : exp relop exp %prec LT"""
+    pass
 
-def p_rbracket_exp(p):
+
+def p_exp_lvalue(p):
+    "exp : lvalue %prec OR"
+    p[0] = p[1]
+
+
+def p_exp_minus(p):
+    "exp : SUB exp"
+    p[0] = NonTerminal()
+    p[0].code = "-" + p[2].code
+
+
+def p_exp_not(p):
+    "exp : NOT exp"
+    p[0] = NonTerminal()
+    p[0].code = "!" + p[2].code
+
+
+def p_exp_rbracket(p):
     "exp : LRB exp RRB"
     p[0] = NonTerminal()
     p[0].code = p[1] + p[2].code + p[3]
 
-def p_lvalue_assign(p):
+
+def p_exp_lvalue_assign(p):
     "exp : lvalue ASSIGN exp"
     p[0] = NonTerminal()
     p[0].code = p[1].code + p[2] + p[3].code
 
 
-def p_expconst(p):
+def p_exp_const(p):
     "exp : const"
     p[0] = NonTerminal()
     p[0].value = p[1]
-    print("p_expconst")
 
 
-def p_binop(p):
+def p_exp_binop(p):
     "exp : exp operator exp %prec MUL"
     CodeGenerator.arithmetic_code(p, new_temp())
 
@@ -239,7 +254,6 @@ def p_operator(p):
     | DIV
     | MOD"""
     p[0] = p[1]
-    print("p_operator")
 
 
 def p_const(p):
@@ -255,24 +269,21 @@ def p_const(p):
         p[0].implicit_type = float
     else:
         p[0].implicit_type = bool
-    print("p_const")
 
 
 def p_explist(p):
     """explist : exp
     | explist COMMA exp
     | eps"""
-    print("p_explist")
+    pass
 
 
 def p_eps(p):
     "eps :"
-    print("p_eps")
-    # pass
+    pass
 
 
 def p_error(p):
-    # print(p.value)
     if p:
         raise Exception("ParsingError: invalid grammar at ", p)
 
